d49c3292030527016bea925498f7991d313e71f5
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-07-02 18:46:38 +0200

    test: drop the favicon

--- test/index.js
+++ test/index.js
@@ -1,14 +1,6 @@
 const test = require('tape');
 const deltaForce = require('../');
 
-// Add favicon
-const linkTag = document.createElement('link');
-
-linkTag.href = 'data:;base64,iVBORw0KGgo=';
-linkTag.rel = 'icon';
-
-document.head.appendChild(linkTag);
-
 test('will return', (t) => {
   t.equal(typeof deltaForce, 'function', 'returns lamda on init');
   t.end();
