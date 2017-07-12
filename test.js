4af865f979a573e155b2490a7e9b07a71fede579
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-07-12 13:13:40 +0200

    test: move one dir up, use kpow, relint

---
+++ test.js
@@ -0,0 +1,16 @@
+require('kpow')()
+
+const test = require('tape')
+const deltaForce = require('./')
+
+test('will return', (t) => {
+  t.equal(typeof deltaForce, 'function', 'returns lamda on init')
+  t.end()
+})
+
+test('will report', (t) => {
+  const data = deltaForce()
+
+  t.equal(data.code, -1, 'nothing')
+  t.end()
+})
