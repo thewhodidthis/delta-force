a9bb74489bb45b173da01019cad014c06ed7a312
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-11-04 20:36:08 +0100

    test: cleanup

--- test.js
+++ test.js
@@ -1,14 +1,16 @@
 import 'cutaway'
 import { report, assert } from 'tapeless'
-
 import deltaForce from './index.es'
 
-const { equal } = assert
+const { equal, ok, notOk } = assert
 
 equal(typeof deltaForce, 'function', 'returns lamda on init', 'will return')
 
-const data = deltaForce()
+const { code, x, y, z } = deltaForce()
 
-equal(data.code, -1, 'nada', 'will report')
+equal(code, -1, 'nada', 'will report')
+notOk(x)
+ok(x === y)
+ok(y === z)
 
 report()
