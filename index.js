7b52e359fc81a3edf1a5dc33414f91467bcd8781
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-12-31 17:53:36 +0100

    module: rewrite

--- index.js
+++ index.js
@@ -1,94 +1,96 @@
 'use strict';
 
-const scratch = [0, 0, 0];
-const bipolar = (a, b, c) => {
-  let memo = [a, b, c];
+var tracker = function (agent) {
+  if ( agent === void 0 ) agent = document;
 
-  return (x, y, z) => {
-    const next = [x, y, z];
-    const diff = memo.map((v, i) => next[i] - v);
+  // For resetting coords
+  var point = [0, 0, 0];
 
-    memo = next;
+  // Holds actual position, gets updated on pointer move
+  var store = point;
 
-    return diff
+  // Holds resulting displacement values
+  var score = point;
+
+  // Button pressed: -1/idle, 0/left, 1/middle, 2/right
+  var state = -1;
+
+  // Alias `addEventListener`, allows for additional listeners outside of
+  // current scope (as opposed to binding handlers inline)
+  var track = agent.addEventListener;
+
+  // Extract location data, whether touch or mouse based
+  var parse = function (ref) {
+    var touches = ref.touches;
+    var clientX = ref.clientX;
+    var clientY = ref.clientY;
+
+    if (touches) {
+      var x = touches[0].pageX;
+      var y = touches[0].pageY;
+
+      // No mouse wheel on touch screens, therefore alias pinch to zoom,
+      // evaluating distance squared between the two fingers
+      var dsq = 0;
+
+      if (state === 1) {
+        var dx = x - touches[1].pageX;
+        var dy = y - touches[1].pageY;
+
+        dsq = (dx * dx) + (dy * dy);
+      }
+
+      return [x, y, dsq]
+    }
+
+    return [clientX, clientY, 0]
+  };
+
+  // Calculate differences, save current position for later
+  var delta = function (e) {
+    var input = parse(e);
+
+    score = store.map(function (v, i) { return input[i] - v; });
+    store = input;
+  };
+
+  // Pointer up
+  var reset = function () {
+    state = -1;
+
+    agent.removeEventListener('mousemove', delta);
+  };
+
+  // Pointer down
+  var start = function (e) {
+    state = e.touches ? e.touches.length - 1 : e.button;
+    store = parse(e);
+
+    track('mousemove', delta);
+    track('touchmove', delta);
+  };
+
+  // Attach event handlers
+  track('touchstart', start);
+  track('mousedown', start);
+  track('touchend', reset);
+  track('mouseup', reset);
+  track('wheel', function (e) {
+    state = 1;
+    score = [0, 0, e.deltaY];
+  });
+
+  // To be called periodically
+  return function () {
+    var x = score[0];
+    var y = score[1];
+    var z = score[2];
+
+    score = point;
+
+    return { x: x, y: y, z: z, state: state }
   }
 };
 
-// -1: idle, 0: left, 1: middle, 2: right
-let state = -1;
-let delta = scratch;
-let force = scratch;
+module.exports = tracker;
 
-const off = document.removeEventListener;
-
-const mouseMove = (e) => {
-  delta = force(e.clientX, e.clientY, 0);
-};
-
-const mouseUp = () => {
-  state = -1;
-
-  off('mouseup', mouseUp);
-  off('mousemove', mouseMove);
-};
-
-const on = document.addEventListener;
-
-on('mousedown', (e) => {
-  state = e.button;
-  force = bipolar(e.clientX, e.clientY, 0);
-
-  on('mouseup', mouseUp);
-  on('mousemove', mouseMove);
-});
-
-const handleTouch = (e, fn) => {
-  const x = e.touches[0].pageX;
-  const y = e.touches[0].pageY;
-
-  let dsq = 0;
-
-  if (state === 1) {
-    const dx = x - e.touches[1].pageX;
-    const dy = y - e.touches[1].pageY;
-
-    dsq = (dx * dx) + (dy * dy);
-  }
-
-  return fn(x, y, dsq)
-};
-
-on('touchstart', (e) => {
-  state = e.touches.length - 1;
-  force = handleTouch(e, bipolar);
-});
-
-on('touchmove', (e) => {
-  // Can't rely on viewport meta tag no more
-  // https://twitter.com/thomasfuchs/status/742531231007559680
-  e.preventDefault();
-
-  delta = handleTouch(e, force);
-});
-
-on('touchend', () => {
-  state = -1;
-});
-
-on('wheel', (e) => {
-  state = 1;
-  delta = [0, 0, e.deltaY];
-}, { passive: true });
-
-const deltaForce = () => {
-  const x = delta[0];
-  const y = delta[1];
-  const z = delta[2];
-
-  // Reset on each call
-  delta = scratch;
-
-  return { x, y, z, code: state }
-};
-
-module.exports = deltaForce;
