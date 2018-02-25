b214ae1df61812addc876b9948708c6a3a424b26
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-25 13:48:02 +0100

    module: go .mjs, buble not the cjs version

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
 'use strict';
 
-var tracker = function (agent) {
-  if ( agent === void 0 ) agent = document;
-
+const tracker = (agent = document) => {
   // For resetting coords
-  var point = [0, 0, 0];
+  const point = [0, 0, 0];
 
   // Holds actual position, gets updated on pointer move
-  var store = point;
+  let store = point;
 
   // Holds resulting displacement values
-  var score = point;
+  let score = point;
 
   // Button pressed: -1/idle, 0/left, 1/middle, 2/right
-  var state = -1;
+  let state = -1;
 
   // Alias `addEventListener`, allows for additional listeners outside of
   // current scope (as opposed to binding handlers inline)
-  var track = agent.addEventListener;
+  const track = agent.addEventListener;
 
   // Extract location data, whether touch or mouse based
-  var parse = function (ref) {
-    var touches = ref.touches;
-    var clientX = ref.clientX;
-    var clientY = ref.clientY;
-
+  const parse = ({ touches, clientX, clientY }) => {
     if (touches) {
-      var x = touches[0].pageX;
-      var y = touches[0].pageY;
+      const x = touches[0].pageX;
+      const y = touches[0].pageY;
 
       // No mouse wheel on touch screens, therefore alias pinch to zoom,
       // evaluating distance squared between the two fingers
-      var dsq = 0;
+      let dsq = 0;
 
       if (state === 1) {
-        var dx = x - touches[1].pageX;
-        var dy = y - touches[1].pageY;
+        const dx = x - touches[1].pageX;
+        const dy = y - touches[1].pageY;
 
         dsq = (dx * dx) + (dy * dy);
       }
@@ -47,22 +41,22 @@ var tracker = function (agent) {
   };
 
   // Calculate differences, save current position for later
-  var delta = function (e) {
-    var input = parse(e);
+  const delta = (e) => {
+    const input = parse(e);
 
-    score = store.map(function (v, i) { return input[i] - v; });
+    score = store.map((v, i) => input[i] - v);
     store = input;
   };
 
   // Pointer up
-  var reset = function () {
+  const reset = () => {
     state = -1;
 
     agent.removeEventListener('mousemove', delta);
   };
 
   // Pointer down
-  var start = function (e) {
+  const start = (e) => {
     state = e.touches ? e.touches.length - 1 : e.button;
     store = parse(e);
 
@@ -75,22 +69,19 @@ var tracker = function (agent) {
   track('mousedown', start);
   track('touchend', reset);
   track('mouseup', reset);
-  track('wheel', function (e) {
+  track('wheel', (e) => {
     state = 1;
     score = [0, 0, e.deltaY];
   });
 
   // To be called periodically
-  return function () {
-    var x = score[0];
-    var y = score[1];
-    var z = score[2];
+  return () => {
+    const [x, y, z] = score;
 
     score = point;
 
-    return { x: x, y: y, z: z, state: state }
+    return { x, y, z, state }
   }
 };
 
 module.exports = tracker;
-
