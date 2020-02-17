43b8556821817aa2db5fb92c86ae78fe07d30448
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2020-02-17 19:53:44 +0000

    test: fix for tapeless latest

--- test.js
+++ test.js
@@ -1,19 +1,27 @@
 import 'cutaway'
-import { report, assert } from 'tapeless'
+import { report, assert } from 'tapeless/browser'
 import deltaForce from './index.mjs'
 
 const { equal, ok, notOk } = assert
 
 const tracker = deltaForce()
 
-equal(typeof tracker, 'function', 'returns lamda on init', 'will return')
+equal
+  .describe('returns lamda on init', 'will return')
+  .test(typeof tracker, 'function')
 
 const { state, x, y, z } = tracker()
 
-equal(state, -1, 'nada', 'will report')
-notOk(x)
+equal
+  .test(state, -1)
 
-ok(x === y)
-ok(y === z)
+notOk
+  .describe('nada')
+  .test(x)
+
+ok
+  .test(x === y)
+  .describe(null, 'will report')
+  .test(y === z)
 
 report()
